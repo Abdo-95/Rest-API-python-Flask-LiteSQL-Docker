f189804b4c4aca5d47a7f8d6d7cde918e54abaaa
arnous@stud.uni-hannover.de <arnous@stud.uni-hannover.de>
2021-09-28 23:30:48 +0000

    db.py

--- db.py
+++ db.py
@@ -5,9 +5,6 @@ conn = sqlite3.connect("Movies.sqlite")
 #database must be according to the data design in our fall we have Movies with 2 Attribiute ID and Movie_Name
 #cursor Objext to excute SQL statments on the SQL database
 cursor = conn.cursor()
-#SQL query needs to be excuted using this 
-sql_query = """ CREATE TABLE Movie (
-    id integer PRIMARY KEY , 
-    Movie_Name text NOT NULL
-    )"""
+#SQL query needs to be excuted using this
+sql_query = """ CREATE TABLE Movie (id integer PRIMARY KEY ,Movie_Name text NOT NULL)"""
 cursor.execute(sql_query)
